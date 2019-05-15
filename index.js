f144757fd17a59e58068c87e335f3be45e3571e7
Hoangpersievn <hoangpersievn@gmail.com>
2019-05-15 16:05:19 +0700

    practice about express

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+let users = [
+    { name : 'chuong', nick : 'chuong'},
+    { name : 'phuc', nick : 'fuck'},
+    { name : 'man', nick : 'man'}
+];
+
+app.listen(port, () => console.log(`server listen on port ${port}`));
+
+app.get('/', (req, res) => {
+    res.render('index.pug', {admin : 'HoangPersievn'});
+})
+
+app.get('/user', (req, res) => {
+    res.render('users/users.pug', {
+        users: users
+    })
+})
+
+
+
