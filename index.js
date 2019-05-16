76755b2a7b8522e2b3abb8ecf0e2b118854ca511
Hoangpersievn <hoangpersievn@gmail.com>
2019-05-16 16:16:03 +0700

    static file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const shortid = require('shortid');
 
 const userRouter = require('./routes/user.route.js');
 
@@ -16,6 +15,7 @@ app.listen(port, () => console.log(`server listen on port ${port}`));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use('/user', userRouter);
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index.pug', {admin : 'HoangPersievn'});
